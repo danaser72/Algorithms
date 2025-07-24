1d0932544d3ca75057487eaf4662235efb91b414
danaser72 <worl5604@naver.com>
2025-07-24 10:25:04 +0900

    단계별 - 2차원 배열 완료 > 배열 초기화에 주의

--- acmicpc/js/js/2563/index.js
+++ acmicpc/js/js/2563/index.js
@@ -1,8 +1,19 @@
 var fs = require("fs");
 // 제출 시, ${__dirname}/ 제거하고 사용
-var input = fs.readFileSync(`${__dirname}/dev/stdin`).toString().trim().split(" ");
-//var input = fs.readFileSync(`${__dirname}/dev/stdin`).toString().trim().split("\n");
-var a = parseInt(input[0]);
-var b = parseInt(input[1]);
+var input = fs.readFileSync(`${__dirname}/dev/stdin`).toString().trim().split("\n");
+// 엄청난 문제가 있는 소스 - 배열이 서로를 참조하게 됨
+//let arr = new Array(100).fill(new Array(100).fill(0));
+let arr = Array.from({ length: 100 }, () => new Array(100).fill(0));
 
-console.log("here", a, b);
+const setPager = (x, y) => {
+  for (let i = x; i < x + 10; i++) {
+    for (let j = y; j < y + 10; j++) {
+      arr[i][j] = 1;
+    }
+  }
+}
+for (let i = 1; i < input.length; i++) {
+  const point = input[i].trim().split(" ").map((x) => parseInt(x));
+  setPager(point[0], point[1]);
+}
+console.log(arr.flat().filter((x) => x === 1).length);
