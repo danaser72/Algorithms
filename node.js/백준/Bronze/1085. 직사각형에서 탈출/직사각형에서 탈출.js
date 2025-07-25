7fa48bdd7ffb81e4b26296724a79338d8be81a99
민재기 <worl5604@naver.com>
2025-07-25 15:07:34 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 88 ms, Memory: 9336 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.js
@@ -0,0 +1,8 @@
+var fs = require("fs");
+var input = fs.readFileSync(`/dev/stdin`).toString().trim().split(" ");
+let x = parseInt(input[0]);
+let y = parseInt(input[1]);
+let w = parseInt(input[2]);
+let h = parseInt(input[3]);
+let result = [Math.abs(w - x), Math.abs(h - y), x, y];
+console.log(Math.min.apply(null, result));
