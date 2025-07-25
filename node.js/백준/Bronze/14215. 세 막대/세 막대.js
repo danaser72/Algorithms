e447553111924c8413252a44ce85a91f03bb8709
민재기 <worl5604@naver.com>
2025-07-25 17:18:34 +0900

    [Bronze III] Title: 세 막대, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/14215. 세 막대/세 막대.js
@@ -0,0 +1,7 @@
+var fs = require("fs");
+var input = fs.readFileSync(`/dev/stdin`).toString().trim().split(" ").map(x => parseInt(x)).sort((a, b) => a - b);
+if (input[0] + input[1] > input[2]) {
+  console.log(input[0] + input[1] + input[2]);
+} else {
+  console.log((input[0] + input[1]) * 2 - 1);
+}
