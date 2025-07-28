2f8339669495bbf8b5a32dcf7e1da323113bc1cb
민재기 <worl5604@naver.com>
2025-07-28 15:54:47 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 92 ms, Memory: 9572 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.js
@@ -0,0 +1,11 @@
+var fs = require("fs");
+var input = fs.readFileSync(`/dev/stdin`).toString().trim().split(" ");
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+const c = parseInt(input[2]);
+const d = parseInt(input[3]);
+const e = parseInt(input[4]);
+const f = parseInt(input[5]);
+const x = parseInt((b * f - c * e) / (b * d - a * e));
+const y = parseInt((a * f - c * d) / (a * e - b * d))
+console.log(`${x} ${y}`);
