8aec365d47c6936ff9d85dd64ba19b91bf5e22dd
민재기 <worl5604@naver.com>
2025-07-29 18:07:34 +0900

    [Silver III] Title: 분수 합, Time: 92 ms, Memory: 9584 KB -BaekjoonHub

---
+++ node.js/백준/Silver/1735. 분수 합/분수 합.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(`/dev/stdin`).toString().trim().split("\n");
+const ab = input[0].split(" ");
+const a = parseInt(ab[0]);
+const b = parseInt(ab[1]);
+const cd = input[1].split(" ");
+const c = parseInt(cd[0]);
+const d = parseInt(cd[1]);
+const gcd = (m, n) => {
+  if (m % n === 0) return n;
+  return gcd(n, m % n);
+};
+const m = a * d + b * c;
+const n = b * d;
+let gcdMN;
+if (m < n) {
+  gcdMN = gcd(m, n);
+} else {
+  gcdMN = gcd(n, m);
+}
+console.log(`${m / gcdMN} ${n / gcdMN}`);
