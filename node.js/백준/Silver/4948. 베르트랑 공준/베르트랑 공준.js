0ebf8d06b053455c1811a0b77ff754bc84ad3579
민재기 <worl5604@naver.com>
2025-07-29 19:12:37 +0900

    [Silver II] Title: 베르트랑 공준, Time: 204 ms, Memory: 13356 KB -BaekjoonHub

---
+++ node.js/백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(`/dev/stdin`).toString().trim().split("\n").map(x => parseInt(x));
+const arr = new Array(246913).fill(0);
+for (let i = 0; i < arr.length; i++) {
+  if (i === 0 || i === 1) arr[i] = false;
+  if (arr[i] === 0) {
+    arr[i] = i;
+    for (let j = 2; i * j < arr.length; j++) {
+      arr[i * j] = false;
+    }
+  }
+}
+const prime = arr.filter((x) => x);
+for (let i = 0; input[i] !== 0; i++) {
+  let cnt = 0;
+  for (let j = 0; prime[j] <= input[i] * 2; j++) {
+    if (prime[j] > input[i]) cnt++;
+  }
+  console.log(cnt);
+}
