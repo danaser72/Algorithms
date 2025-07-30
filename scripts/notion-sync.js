e73e19d6215d6ee923bcec6ab7b129daa8f29c52
민재기 <worl5604@naver.com>
2025-07-30 18:03:55 +0900

    Update notion-sync.js

--- scripts/notion-sync.js
+++ scripts/notion-sync.js
@@ -42,7 +42,7 @@ async function getPageContent(pageId) {
 
 (async () => {
   const pages = await fetchAllPages();
-  const outputDir = "Notion/study";
+  const outputDir = "study";
 
   for (const page of pages) {
     const props = page.properties;
