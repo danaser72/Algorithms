1df7d13b3f43fccf7db83997cbe1192cc5a6a8cf
민재기 <worl5604@naver.com>
2025-07-31 18:17:49 +0900

    [Silver IV] Title: 괄호, Time: 96 ms, Memory: 9496 KB -BaekjoonHub

---
+++ node.js/백준/Silver/9012. 괄호/괄호.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(`/dev/stdin`).toString().trim().split("\n");
+const check = (str) => {
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (stack.length === 0) stack.push(str[i]);
+    else {
+      if (stack[stack.length - 1] === "(" && str[i] === ")"){
+        stack = stack.slice(0, stack.length - 1);
+      } else{
+         stack.push(str[i]);
+      }
+    }
+  }
+  return stack.length === 0 ? "YES" : "NO";
+};
+const result = [];
+for (let i = 1; i < input.length; i++) {
+  result.push(check(input[i].trim()));
+}
+console.log(result.join("\n"));
