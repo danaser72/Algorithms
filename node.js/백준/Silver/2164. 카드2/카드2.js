90a9de9aecde429bea397c40541ae277f4b7ed7e
민재기 <worl5604@naver.com>
2025-07-31 19:02:28 +0900

    [Silver IV] Title: 카드2, Time: 208 ms, Memory: 41304 KB -BaekjoonHub

---
+++ node.js/백준/Silver/2164. 카드2/카드2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(`/dev/stdin`).toString().trim();
+const N = parseInt(input);
+const queue = [];
+let cur = 0;
+for (let i = 1; i <= N; i++) {
+  queue.push(i);
+}
+let action = false;
+while (queue.length - cur !== 1) {
+  const card = queue[cur++];
+  if (action) queue.push(card);
+  action = !action;
+}
+console.log(queue[cur]);
