38f07ab3ab02dbb76a1a9aed4d391255f7025a81
민재기 <worl5604@naver.com>
2025-07-31 18:31:23 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 212 ms, Memory: 14680 KB -BaekjoonHub

---
+++ node.js/백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(`/dev/stdin`)
+  .toString()
+  .trim()
+  .split("\n");
+const conditions = { "(": true, ")": true, "[": true, "]": true };
+const check = (str) => {
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ".") break;
+    if (conditions[str[i]]) {
+      if (stack.length === 0) stack.push(str[i]);
+      else {
+        if (
+          (stack[stack.length - 1] === "(" && str[i] === ")") ||
+          (stack[stack.length - 1] === "[" && str[i] === "]")
+        )
+          stack = stack.slice(0, stack.length - 1);
+        else stack.push(str[i]);
+      }
+    }
+  }
+  return stack.length === 0 ? "yes" : "no";
+};
+const result = [];
+for (let i = 0; i < input.length; i++) {
+  const str = input[i];
+  if (str === ".") break;
+  else {
+    result.push(check(str));
+  }
+}
+console.log(result.join("\n"));
