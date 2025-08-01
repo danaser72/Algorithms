d94d95c34b28bd6a1c7b798ba47d38cacd30d84b
민재기 <worl5604@naver.com>
2025-08-01 18:13:14 +0900

    [Silver V] Title: 다리 놓기, Time: 152 ms, Memory: 10828 KB -BaekjoonHub

---
+++ node.js/백준/Silver/1010. 다리 놓기/다리 놓기.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(`/dev/stdin`)
+  .toString()
+  .trim()
+  .split("\n");
+const fac = (num) => {
+  let result = num === 0n ? 1n : num;
+  for (let i = 2n; i < num; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+let result = "";
+for (let i = 1; i < input.length; i++) {
+  const NM = input[i].split(" ");
+  const N = BigInt(NM[0]);
+  const M = BigInt(NM[1]);
+  result += `${BigInt(fac(M) / (fac(N) * fac(M - N)))}\n`;
+}
+console.log(result.trim());
