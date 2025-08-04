97009a57b681ce25e3d84289b0c63570b44ecf1d
민재기 <worl5604@naver.com>
2025-08-04 17:24:35 +0900

    [Gold IV] Title: N-Queen, Time: 23912 ms, Memory: 50128 KB -BaekjoonHub

---
+++ node.js/백준/Gold/9663. N－Queen/N－Queen.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const N = parseInt(fs.readFileSync(`/dev/stdin`).toString());
+let board = Array.from({ length: N }, () => new Array(N).fill(0));
+let cnt = 0;
+const chess = (board, cur) => {
+  if (cur === N) {
+    cnt++;
+    return;
+  }
+  for (let i = 0; i < N; i++) {
+    if (board[cur][i] === 1) continue;
+    const tmp = board.map((x) => x.slice());
+    draw(cur, i, board);
+    chess(board, cur + 1);
+    board = tmp;
+  }
+};
+
+const draw = (m, n, board) => {
+  board[m][n] = 1;
+  for (let i = 0; i < N; i++) {
+    board[m][i] = 1;
+    board[i][n] = 1;
+  }
+  for (let i = 1; m - i >= 0 && n - i >= 0; i++) {
+    board[m - i][n - i] = 1;
+  }
+  for (let i = 1; m - i >= 0 && n + i < N; i++) {
+    board[m - i][n + i] = 1;
+  }
+  for (let i = 1; m + i < N && n - i >= 0; i++) {
+    board[m + i][n - i] = 1;
+  }
+  for (let i = 1; m + i < N && n + i < N; i++) {
+    board[m + i][n + i] = 1;
+  }
+};
+
+chess(board, 0);
+console.log(cnt);
