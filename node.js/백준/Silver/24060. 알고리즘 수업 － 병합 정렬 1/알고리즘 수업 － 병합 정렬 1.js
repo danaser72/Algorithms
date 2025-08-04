2d431ba665293b84fd815eb04597286e92e0922f
민재기 <worl5604@naver.com>
2025-08-04 13:34:03 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 608 ms, Memory: 153160 KB -BaekjoonHub

---
+++ node.js/백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(`/dev/stdin`)
+  .toString()
+  .trim()
+  .split("\n");
+
+let [N, K] = input[0].split(" ").map((x) => parseInt(x));
+const arr = input[1].split(" ").map((x) => parseInt(x));
+
+const merge_sort = (A, p, r) => {
+  if (p < r) {
+    let q = parseInt((p + r) / 2);
+    merge_sort(A, p, q);
+    merge_sort(A, q + 1, r);
+    merge(A, p, q, r);
+  }
+};
+
+const merge = (A, p, q, r) => {
+  let i = p,
+    j = q + 1,
+    t = 0;
+  let tmp = [];
+  while (i <= q && j <= r) {
+    if (A[i] <= A[j]) {
+      tmp[t++] = A[i++];
+    } else {
+      tmp[t++] = A[j++];
+    }
+  }
+  while (i <= q) {
+    tmp[t++] = A[i++];
+  }
+  while (j <= r) {
+    tmp[t++] = A[j++];
+  }
+  i = p;
+  t = 0;
+  while (i <= r && K !== 0) {
+    A[i++] = tmp[t++];
+    K--;
+
+    if (K === 0) {
+      console.log(A[i - 1]);
+    }
+  }
+};
+
+merge_sort(arr, 0, arr.length - 1);
+if (K !== 0) {
+  console.log(-1);
+}
