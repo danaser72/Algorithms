4293e632d723e7f65408fd95ce14cb6af7c39f22
민재기 <worl5604@naver.com>
2025-08-04 13:21:00 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 164 ms, Memory: 11636 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(`/dev/stdin`)
+  .toString()
+  .trim()
+  .split("\n");
+
+const recursion = (s, l, r) => {
+  cnt++;
+  if (l >= r) return 1;
+  else if (s[l] !== s[r]) return 0;
+  else return recursion(s, l + 1, r - 1);
+};
+
+const isPalidrome = (s) => {
+  return recursion(s, 0, s.length - 1);
+};
+
+let result = "";
+let cnt;
+for (let i = 1; i < input.length; i++) {
+  cnt = 0;
+  result += `${isPalidrome(input[i].trim())} ${cnt}\n`;
+}
+console.log(result.trim());
