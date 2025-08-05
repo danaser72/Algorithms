5d5fa497a0d0dc71691a3b43c4f14be24222ee79
민재기 <worl5604@naver.com>
2025-08-05 18:23:56 +0900

    [Silver III] Title: 파도반 수열, Time: 88 ms, Memory: 9596 KB -BaekjoonHub

---
+++ node.js/백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(`/dev/stdin`).toString().trim().split("\n").map(x => parseInt(x));
+const arr = [];
+arr.push(0)
+arr.push(1)
+arr.push(1)
+const DP = (N) => {
+  for (let i = 3; i <= N; i++) {
+    arr[i] = arr[i - 2] + arr[i - 3];
+  }
+};
+DP(Math.max.apply(null, input))
+let result = "";
+for (let i = 1; i< input.length;i++){
+    const N = parseInt(input[i]);
+    result += `${arr[N]}\n`;
+}
+console.log(result.trim());
