14475e1eae86c9319fd8b3e8c12833ee9d1ee099
민재기 <worl5604@naver.com>
2025-08-06 17:13:20 +0900

    [Gold V] Title: 전깃줄, Time: 92 ms, Memory: 9380 KB -BaekjoonHub

---
+++ node.js/백준/Gold/2565. 전깃줄/전깃줄.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(`/dev/stdin`)
+  .toString()
+  .trim()
+  .split("\n");
+const N = parseInt(input[0]);
+const arr = input
+  .slice(1)
+  .map((x) => x.split(" ").map((y) => parseInt(y)))
+  .sort((a, b) => a[0] - b[0]);
+
+const LCS = [];
+for (let i = 0; i < arr.length; i++) {
+  const num = arr[i][1];
+  let score = 1;
+  for (let j = 0; j < i; j++) {
+    if (arr[j][1] < num) {
+      score = Math.max(LCS[j] + 1, score);
+    }
+  }
+  LCS.push(score);
+}
+console.log(N - Math.max.apply(null, LCS));
