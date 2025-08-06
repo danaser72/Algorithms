c2469f473be1eedc441c0d831dd47fdde6debf26
민재기 <worl5604@naver.com>
2025-08-06 11:21:28 +0900

    [Silver I] Title: 정수 삼각형, Time: 192 ms, Memory: 16288 KB -BaekjoonHub

---
+++ node.js/백준/Silver/1932. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(`/dev/stdin`)
+  .toString()
+  .trim()
+  .split("\n");
+const DP = [];
+for (let i = 1; i < input.length; i++) {
+  const arr = input[i]
+    .trim()
+    .split(" ")
+    .map((x) => parseInt(x));
+  if (i === 1) {
+    DP.push(arr.slice());
+  } else {
+    const _arr = [];
+    for (let j = 0; j < arr.length; j++) {
+      if (j === 0) {
+        _arr.push(DP[DP.length - 1][j] + arr[j]);
+      } else if (j === arr.length - 1) {
+        _arr.push(DP[DP.length - 1][j - 1] + arr[j]);
+      } else {
+        _arr.push(
+          Math.max(DP[DP.length - 1][j - 1], DP[DP.length - 1][j]) + arr[j]
+        );
+      }
+    }
+    DP.push(_arr);
+  }
+}
+console.log(Math.max.apply(null, DP[DP.length - 1]));
