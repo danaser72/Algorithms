22c05cd1d4d7d0550f838aa7d1066d597b003904
민재기 <worl5604@naver.com>
2025-08-08 14:28:40 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 600 ms, Memory: 76564 KB -BaekjoonHub

---
+++ node.js/백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(`/dev/stdin`)
+  .toString()
+  .trim()
+  .split("\n");
+const [N, M] = input[0].split(" ").map((x) => parseInt(x));
+const sum = [new Array(N + 1).fill(0)];
+for (let i = 1; i < N + 1; i++) {
+  const arr = [0].concat(input[i].split(" ").map((x) => parseInt(x)));
+  for (let j = 1; j < arr.length; j++) {
+    arr[j] = arr[j - 1] + arr[j] + sum[i - 1][j] - sum[i - 1][j - 1];
+  }
+  sum.push(arr);
+}
+let result = "";
+for (let i = N + 1; i < N + M + 1; i++) {
+  const [x1, y1, x2, y2] = input[i].split(" ").map((x) => parseInt(x));
+  result += `${
+    sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]
+  }\n`;
+}
+console.log(result.trim());
