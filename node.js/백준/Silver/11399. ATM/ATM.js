7962295a3084f7ff847924bc3d648466f2fcf41c
민재기 <worl5604@naver.com>
2025-08-08 18:44:26 +0900

    [Silver IV] Title: ATM, Time: 100 ms, Memory: 9652 KB -BaekjoonHub

---
+++ node.js/백준/Silver/11399. ATM/ATM.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(`/dev/stdin`)
+  .toString()
+  .trim()
+  .split("\n");
+const arr = input[1].split(" ").map((x) => parseInt(x)).sort((a, b) => a - b);
+const sum = [0];
+for (let i = 1; i <= arr.length; i++) {
+    sum[i] = sum[i - 1] + arr[i - 1];
+}
+console.log(sum.reduce((acc, cur) => acc + cur, 0))
